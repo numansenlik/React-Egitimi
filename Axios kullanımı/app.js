1bd129e7c519a8c20768a18dc1f016d9b51b4da0
Numan Şenlik <nmnsnlk@gmail.com>
2023-06-07 00:14:25 +0300

    React Başangıç Bilgileri

--- Axios kullanımı/app.js
+++ Axios kullanımı/app.js
@@ -1,2 +1,39 @@
-let a = 0
-let b = 1
+// ! Fech api ya göre direk json formatında dosyayı çekiyoruz birddaa json()dememize gerek kalmıyor.
+// const veri = async()=>{
+//     let {data: users} = await axios("https://jsonplaceholder.typicode.com/posts");
+//     users.forEach(element => {
+//         console.log(element.title)
+//     });
+// }
+// veri()
+
+
+
+// const getUsers = ()=>{
+// return new Promise(async(resolve,reject)=>{
+//     const { data } = await axios("https://jsonplaceholder.typicode.com/users");
+//     resolve(data)
+// })}
+
+// const getPost = ()=>{
+//     return new Promise(async(resolve,reject)=>{
+//         const {data :post} = await axios("https://jsonplaceholder.typicode.com/posts/1");
+//         resolve(post)
+// })}
+
+
+// (async ()=> {
+//     try {
+//         const post = await getPost();
+//         const user = await getUsers();
+
+//         console.log(user);
+//         console.log(post);
+//     } catch (error) {
+//         console.log(error);
+//     }
+
+// })();
+// Promise.all([getUsers(),getPost()])
+// .then(console.log)
+// .catch(console.log)
