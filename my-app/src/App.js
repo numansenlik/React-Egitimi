7aaa15612ea4348e8e11a0ecbcdef85863301bad
Numan Şenlik <nmnsnlk@gmail.com>
2023-06-08 03:11:58 +0300

    Components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import React from'react';
+import Header from './components/Header';
+const name ="Numan";
+const surname="Şenlik";
+const isLoggedIn = true;
+function App (){
+  // return React.createElement('div',null,'Hello')
+// return(<div>
+  {/* <Header/> */}
+  {/* <Header/> */}
+  {/* <Header/> */}
+{/* </div>) */}
+// return (<div><h1>{`Benim Adım ${name} Soyadım ${surname}`}</h1></div>)
+return (<React.Fragment><h1>{isLoggedIn ? `Benim Adım ${name} Soyadım ${surname}`:`Giriş Yapmadınız`}</h1>
+{/* <label htmlFor="input">Numan</label> */}
+{/* <input id="input"/> */}
+</React.Fragment>)
 }
-
 export default App;
---
+++ my-app/src/components/Header.js
@@ -0,0 +1,4 @@
+function Header(){
+    return <div>Merhaba ben Header componentiyim</div>
+}
+export default Header;
