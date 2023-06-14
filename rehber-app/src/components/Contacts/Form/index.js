53267366433e4edc0c559f06b81fbf21db83e559
Numan Şenlik <nmnsnlk@gmail.com>
2023-06-15 00:10:49 +0300

    Final

--- rehber-app/src/components/Contacts/Form/index.js
+++ rehber-app/src/components/Contacts/Form/index.js
@@ -1,27 +1,29 @@
 import React, { useEffect, useState } from 'react'
 
-function Form({directory, setDirectory }) {
+function Form({ directory, setDirectory }) {
     const [info, setInfo] = useState({ name: "", number: "" });
     const callInfo = (e) => {
         setInfo({ ...info, [e.target.name]: e.target.value })
     }
     const addInfo = (e) => {
         e.preventDefault();
-        if (!(info.name === ""  || info.number === "")) {
+        if (!(info.name === "" || info.number === "")) {
             setDirectory([...directory, info]);
-            setInfo({name:"",number:""})
+            setInfo({ name: "", number: "" })
         }
     }
 
     return (<React.Fragment>
         <form onSubmit={addInfo}>
             <div>
-                <input name='name' type='text' placeholder='Pls Name Write' onChange={callInfo} value={info.name}/>
+                <input name='name' type='text' placeholder='Pls Name Write' onChange={callInfo} value={info.name} />
             </div>
             <div>
                 <input name='number' type='number' placeholder='Pls Number Write' onChange={callInfo} value={info.number} />
             </div>
-            <button>Add</button>
+            <div className='btn'>
+                <button>Add</button>
+            </div>
         </form>
     </React.Fragment>
     )
