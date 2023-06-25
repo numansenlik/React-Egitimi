0ca19862ba22713c1fcb275915db27171784a1f2
Numan Şenlik <nmnsnlk@gmail.com>
2023-06-25 17:19:32 +0300

    use location

--- react-router/src/pages/Users.js
+++ react-router/src/pages/Users.js
@@ -1,20 +1,20 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 function Users() {
-  const [users,setUsers] = useState([]);
-useEffect(()=>{
-axios("https://jsonplaceholder.typicode.com/users")
-.then((data)=>{setUsers(data.data)})
-},[])
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    axios("https://jsonplaceholder.typicode.com/users")
+      .then((data) => { setUsers(data.data) })
+  }, [])
   return (
     <div>
-    <h2>Kulanıcılar</h2>
-    <ul>
-    {users.map((user)=><li key={user.id}>
-      <Link to={`${user.id}`}>{user.name}</Link>
-    </li>)}
-    </ul>
+      <h2>Kulanıcılar</h2>
+      <ul>
+        {users.map((user) => <li key={user.id}>
+          <Link to={`${user.id}`} state={user}>{user.name}</Link>
+        </li>)}
+      </ul>
     </div>
   )
 }
